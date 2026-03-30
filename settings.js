43dcd1469e3a4926c530e943c56fa9e80c936b46
naoygzn21-gif <nao.ygzn21@gmail.com>
2026-03-31 00:11:50 +0900

    Add files via upload

--- settings.js
+++ settings.js
@@ -1,16 +1,18 @@
 /* =====================================================
    settings.js — おうちごはん帖 サイト設定
-   更新日時: 2026/3/30 23:30:06
+   更新日時: 2026/3/31 0:10:48
    ===================================================== */
 var SETTINGS_DATA = {
   "adminEmail": "admin@example.com",
-  "adminPw": "admin1234",
+  "adminPw": "kaikuu08",
   "themeCss": "",
   "sections": {
     "gallery": true
   },
   "instagramPosts": [
     "https://www.instagram.com/p/DWeTWqUASRB/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
-    "https://www.instagram.com/p/DWeTHAgAXoo/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
+    "https://www.instagram.com/p/DWeTHAgAXoo/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    "https://www.instagram.com/p/DWgy-z8gREM/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==",
+    "https://www.instagram.com/p/DWg0hVXAS4I/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA=="
   ]
 };
